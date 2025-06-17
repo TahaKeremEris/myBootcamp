e852e4cf381177ec639249b3575a2d1d7733faf2
Taha Kerem ERİŞ <taha.eris@payco.com.tr>
2025-06-17 09:23:02 +0300

    Ufak değişiklik ve projenin son hali

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,7 +21,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        return RedirectToAction("Merhaba");
     }
 
     public IActionResult Privacy()
